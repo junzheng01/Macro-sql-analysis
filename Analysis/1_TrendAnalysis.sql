b7e03b94632573b78e5f83e4c44db9cc19b4b4b7
junzheng01 <junzheng0624@gmail.com>
2025-07-15 16:20:35 -0400

    Update and rename Q1.sql to 1_TrendAnalysis.sql

--- Analysis/1_TrendAnalysis.sql
+++ Analysis/1_TrendAnalysis.sql
@@ -1,4 +1,4 @@
--- Compare GDP or inflation between consecutive years (year vs year-1) 
+-- Q: Compare GDP or inflation between consecutive years (year vs year-1) 
 -- USA from 2001 - 2024 , since 2000 has no data to compare to, I chose to remove that row
 -- Skills:  Advanced Joins 
 SELECT
@@ -9,5 +9,12 @@ SELECT
 FROM macro_gdp curr
 LEFT JOIN macro_gdp prev 
 ON curr.year = prev.year + 1
-ORDER BY curr.year
-offset 1;
+ORDER BY curr.year;
+
+
+--Q: How has GDP growth (rGDP) trended over the last 10 years?
+
+SELECT year , 
+ROUND((rgdp - LAG(rgdp, 1) OVER(ORDER BY year) )*100 / rgdp , 2) GDP_Change_Pct
+from macro_gdp
+offset 1; 
