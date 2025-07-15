6776409203532af13e20b0e985bdf6b33481ea91
junzheng01 <junzheng0624@gmail.com>
2025-07-15 16:46:10 -0400

    Update 1_TrendAnalysis.sql

--- Analysis/1_TrendAnalysis.sql
+++ Analysis/1_TrendAnalysis.sql
@@ -23,5 +23,16 @@ SELECT ROUND(avg(gdp_change_pct), 2) Pct_10Years
 from yearly_gdp_stats
 where year BETWEEN 2014 and 2024
 ; 
-
 -- A : rGDP has averaged a 2.26% growth 
+
+-- Rank years by inflation rate and highlight years in the top 10%.
+-- SKills : RANK(), PERCENT_RANK(), CTE 
+WITH percentile AS (select RANK() OVER (ORDER BY infl_pct DESC) , year , infl_pct,
+PERCENT_RANK() OVER (ORDER BY infl_pct) AS percentile_infl
+from macro_years)
+
+SELECT * 
+from percentile
+where percentile_infl >= .90
+ORDER BY rank asc
+;
