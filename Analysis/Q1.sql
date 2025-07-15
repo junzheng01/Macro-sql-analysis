5a4cafcc795a6f91746d1ad7c3980d2836c883f7
junzheng01 <junzheng0624@gmail.com>
2025-07-15 15:59:27 -0400

    Create Q1.sql

---
+++ Analysis/Q1.sql
@@ -0,0 +1,11 @@
+-- Compare GDP or inflation between consecutive years (year vs year-1)
+SELECT
+  curr.year,
+  curr.rGDP,
+  prev.rGDP AS prev_rGDP,
+  curr.rGDP - prev.rGDP AS rGDP_change
+FROM macro_gdp curr
+LEFT JOIN macro_gdp prev 
+ON curr.year = prev.year + 1
+ORDER BY curr.year
+offset 1;
