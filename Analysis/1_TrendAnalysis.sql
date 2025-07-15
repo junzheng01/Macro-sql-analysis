b948a335b1bf46d08da850e7e350bf002dfb1e5b
junzheng01 <junzheng0624@gmail.com>
2025-07-15 16:29:54 -0400

    Update 1_TrendAnalysis.sql

--- Analysis/1_TrendAnalysis.sql
+++ Analysis/1_TrendAnalysis.sql
@@ -13,8 +13,15 @@ ORDER BY curr.year;
 
 
 --Q: How has GDP growth (rGDP) trended over the last 10 years?
+--Skills: CTE + Window Functions LAG() 
 
-SELECT year , 
+WITH yearly_gdp_stats AS(SELECT year , rgdp, LAG(rgdp, 1) OVER(ORDER BY year) prev_rgdp,
 ROUND((rgdp - LAG(rgdp, 1) OVER(ORDER BY year) )*100 / rgdp , 2) GDP_Change_Pct
-from macro_gdp
-offset 1; 
+from macro_gdp)
+
+SELECT ROUND(avg(gdp_change_pct), 2) Pct_10Years
+from yearly_gdp_stats
+where year BETWEEN 2014 and 2024
+; 
+
+-- A : rGDP has averaged a 2.26% growth 
