69637c30c71ac7cea5abe8efd77ec5cfe6c01fda
junzheng01 <junzheng0624@gmail.com>
2025-08-07 18:08:57 -0400

    Update 3_UnemploymentTrend.sql

--- Analysis/3_UnemploymentTrend.sql
+++ Analysis/3_UnemploymentTrend.sql
@@ -1,4 +1,4 @@
---Q1. Has unemployment returned to pre-crisis levels? 
+--Q1: Has unemployment returned to pre-crisis levels? 
 -- Skill: Window function LAG(), CTE
 WITH unemp_trend AS (
   SELECT
@@ -19,4 +19,30 @@ WHERE year IN (2008, 2009, 2020, 2021);
 --          assess how long it took for unemployment to rebound
 
 
+--Q.2:Which years had the highest average monthly unemployment?
+-- Skill: Aggregation, Grouping, JOIN
+SELECT
+  year,
+  AVG(unemployment_rate) AS avg_monthly_unemp 
+FROM unemployment_long
+GROUP BY year
+ORDER BY avg_monthly_unemp DESC
+LIMIT 5
+;
 
+-- Insight: Identifies the top 5 years with the highest avg monthly unemployment rates
+--          Helpful in spotting prolong economic distress 
+
+-- Q3: How closely does real GDP growth predict changes in the unemployment rate?”
+-- Skill: Window functions (LAG()), joins, computed fields
+SELECT
+  m.year,
+  g.rgdp,
+  g.rgdp - LAG(g.rgdp) OVER (ORDER BY m.year) AS rgdp_change,
+  m.unemp_pct,
+  m.unemp_pct - LAG(m.unemp_pct) OVER (ORDER BY m.year) AS unemp_change
+FROM macro_years m
+JOIN macro_gdp g ON m.year = g.year
+ORDER BY m.year;
+
+--Insight: Shows year-by-year relationships between real GDP growth and unemployment rate changes
