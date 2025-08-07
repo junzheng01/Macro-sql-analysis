8aa82966f373716f28a3d1f4cd02464b5020a32f
junzheng01 <junzheng0624@gmail.com>
2025-08-07 16:45:15 -0400

    Update 1_TrendAnalysis.sql

--- Analysis/1_TrendAnalysis.sql
+++ Analysis/1_TrendAnalysis.sql
@@ -1,4 +1,4 @@
--- Q: Compare GDP (rGDP, accounts for inflation) between consecutive years (2000-2024) 
+-- Q.1: Compare GDP (rGDP, accounts for inflation) between consecutive years (2000-2024) 
 -- Skills:  Advanced Joins 
 SELECT
   curr.year,
@@ -20,9 +20,34 @@ ORDER BY curr.year;
 --- 2020: rgdp_change: -410526.00
 --- 2021: rgdp_change:1051938.00
 
+---Q.1.A: Compare the rgdp_change with the average rgdp_change 
+---       (excludes the negative as crisises are not a "normal" account of rgdp) 
+SELECT 
+AVG(rgdp_change) AS rgdp_avg
+FROM(
+SELECT
+curr.year, 
+CASE 
+WHEN curr.rGDP - prev.rGDP > 0 THEN curr.rGDP - prev.rGDP
+ELSE 0
+END AS rgdp_change
+FROM macro_gdp curr
+LEFT JOIN macro_gdp prev 
+ON curr.year = prev.year + 1
+ORDER BY curr.year
+OFFSET 1
+);
+--- rgdp_avg : 363934.333333333333
 
 
---Q: How has GDP growth (rGDP) trended over the last 10 years?
+
+
+
+
+
+
+
+--Q.2: How has GDP growth (rGDP) trended over the last 10 years?
 --Skills: CTE + Window Functions LAG() 
 
 WITH yearly_gdp_stats AS(SELECT year , rgdp, LAG(rgdp, 1) OVER(ORDER BY year) prev_rgdp,
